4bb5f9c0bf21c1d72eea0d6eabcf756c305a7141
Grenby <Grenby@users.noreply.github.com>
2020-10-27 10:05:29 +0300

    add incorrect T3 from 8 contest:)

--- contests/c7/T3.cpp
+++ contests/c7/T3.cpp
@@ -7,8 +7,8 @@ typedef list<char>::iterator & pos;
 
 p E(pos iter); // S(P) считате результат выражения (сумма полиномов)
 p S(pos iter,int64_t  value); // +-S(P) добавляет или вычитает к value следующий полином
-p P(pos iter); // UM - счиатет последовательность умножений или делений
-p M(pos iter,int64_t  value); // делит или умножает value на UM
+p P(pos iter); // UM - счиатет последовательность умножений или делений (полиномы)
+p M(pos iter,int64_t  value); // делит или умножает value на UM (рекурсивно считает значение полинома)
 p U(pos iter); // унарный минус от числа или выражения в скобках
 p T(pos iter); // дает число или результат выражения в скобках
 p N(pos iter); // собирает число
@@ -31,34 +31,34 @@ p S(pos iter,int64_t value){
 }
 
 p P(pos iter){
-    auto u = U(iter);
-    return u.first ? M(iter,u.second) : u;
+    auto _u = U(iter);
+    return _u.first ? M(iter, _u.second) : _u;
 }
 
 p M(pos iter,int64_t value){
     if (*iter == '*' || *iter == '/') {
         bool ml = (*iter == '*');
-        auto u = U(++iter);
-        if (!u.first || (!ml && u.second == 0))return u;
-        return M(iter,ml ? value * u.second : value / u.second);
+        auto _u = U(++iter);
+        if (!_u.first || (!ml && _u.second == 0))return _u;
+        return M(iter, ml ? value * _u.second : value / _u.second);
     }return p{true, value};
 }
 
 p U (pos iter){
     if (*iter == '-'){
-        auto u = U(++iter);
-        u.second = - u.second;
-        return u;
+        auto _u = U(++iter);
+        _u.second = - _u.second;
+        return _u;
     }
     return T(iter);
 }
 
 p T(pos iter){
     if (*iter == '('){
-        auto e = E(++iter);
+        auto _e = E(++iter);
         if (*iter != ')')return p{false,0};
         ++iter;
-        return e;
+        return _e;
     }else return N(iter);
 }
 
